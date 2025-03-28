14653f88043f85929e1c7d9423814e2f507634c4
alozoya <alozoyaj@minsait.com>
2025-03-28 23:22:27 +0100

    send_email

--- agent/src/send_email_alba.py
+++ agent/src/send_email_alba.py
@@ -20,26 +20,15 @@ logging.basicConfig(
 
 class CreateMessagesPipeline:
     """
-    Pipeline to make a review of water analysis results and send an email.
+    This pipeline sends an email with the provided body text to a list of receivers.
     """
 
     def __init__(self):
         self.sender_email = "ibm.hackaton.ai@gmail.com"
-        self.app_password = "zslv iafx oihh kbsm"  # Usa una contraseña de aplicación de Gmail
+        self.app_password = "zslv iafx oihh kbsm" 
         self.receivers = ["citycouncilspain@gmail.com"]
         self.subject = "Anomaly Detected in Water Tank Sensor Data"
         
-        model_gpt4 = ModelFactory.factory_method(
-            temperature=0.0,
-            model="gpt-4",
-        )
-
-        prompt = PromptFactory.factory_method(
-            domain="describe_water_data",
-        )["describe_water_data"]["PROMPT"]
-
-        self.analysis_chain = prompt | model_gpt4
-        logging.info("DescribeWaterGPT initialized successfully.")
 
     def send_email(self, body: str):
         """
@@ -66,13 +55,13 @@ class CreateMessagesPipeline:
         except Exception as e:
             logging.error(f"Failed to send email: {e}")
 
-    def invoke(self, water_info: str) -> str:
-        """
-        Creates the message and sends an email.
-        """
-        response = self.analysis_chain.invoke({"water_data": water_info})
+    # def invoke(self, water_info: str) -> str:
+    #     """
+    #     Creates the message and sends an email.
+    #     """
+    #     response = self.analysis_chain.invoke({"water_data": water_info})
 
-        # Send email with the generated response
-        self.send_email(response)
+    #     # Send email with the generated response
+    #     self.send_email(response)
 
-        return response
+    #     return response
